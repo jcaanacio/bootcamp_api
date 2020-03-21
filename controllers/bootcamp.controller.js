f2619bf3e2491f32cd1b18fb4e98232ae2943fff
JayChristopherAnacio <jcaanacio@gmail.com>
2020-03-21 20:45:46 +0800

    slugify and hooks, asnyc handlers and error handler middleware

--- controllers/bootcamp.controller.js
+++ controllers/bootcamp.controller.js
@@ -1,93 +1,68 @@
 const ErrorResponse = require('../utils/ErrorResponse');
+const AsyncHandler = require('../middleware/asyncHandler');
 class BootcampController {
 
     constructor(bootcampService) {
         this._bootcampService = bootcampService;
     }
 
-    get = async (request, response, next) => {
-        try {
-            const bootcamps = await this._bootcampService.getAllBootcamps();
-            response.status(200).json({
-                success:true,
-                message: `List of bootcamps`,
-                count: bootcamps.length,
-                body: bootcamps
-            });
-        } catch (err) {
-            response.status(400).json({sucess: false});
+    get = AsyncHandler( async (request, response, next) => {
+        const bootcamps = await this._bootcampService.getAllBootcamps();
+        response.status(200).json({
+            success:true,
+            message: `List of bootcamps`,
+            count: bootcamps.length,
+            body: bootcamps
+        });
+    });
+
+    getById = AsyncHandler(async (request, response, next) => {
+        const bootcamp = await this._bootcampService.getBootcampById(request.params.id);
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with the id of ${request.params.id}`, 404));
         }
         
-    }
+        return response.status(200).json({
+            success:true,
+            message: `List of bootcamp`,
+            body: bootcamp
+        });
+    });
 
-    getById = async (request, response, next) => {
-        try {
-            const bootcamp = await this._bootcampService.getBootcampById(request.params.id);
-            if (!bootcamp) {
-                // return response.status(400).json({sucess: false});
-                return next(new ErrorResponse(`Bootcamp not found with the id of ${request.params.id}`, 404));
-            }
-            
-            return response.status(200).json({
-                success:true,
-                message: `List of bootcamp`,
-                body: bootcamp
-            });
-        } catch (err) {
-            // response.status(400).json({sucess: false});
-            next(new ErrorResponse(err.message, 400));
+    create = AsyncHandler(async (request, response, next) => {
+        const bootcamp = await this._bootcampService.create(request.body);
+        response.status(200).json({
+            success: true,
+            message: `Created new bootcamp`,
+            body: bootcamp
+        });
+    });
+
+    updateById = AsyncHandler(async (request, response, next) => {
+        const bootcamp = await this._bootcampService.updateById(request.params.id,request.body);
+        if (!bootcamp) {
+            return response.status(400).json({sucess: false , body: bootcamp});
         }
-    };
 
-    create = async (request, response, next) => {
-        try {
-            const bootcamp = await this._bootcampService.create(request.body);
-            response.status(200).json({
-                success: true,
-                message: `Created new bootcamp`,
-                body: bootcamp
-            });
-        } catch (err) {
-            response.status(400).json({sucess: false});
+        response.status(200).json({
+            success:true,
+            message: `Updated Bootcamp ${request.params.id}`,
+            body: bootcamp
+        });
+    });
+
+    deleteById = AsyncHandler(async (request, response, next) => {
+        const bootcamp = await this._bootcampService.deleteById(request.params.id);
+        if (!bootcamp) {
+            return response.status(400).json({sucess: false , body: bootcamp});
         }
-        
-    };
 
-    updateById = async (request, response, next) => {
-        
-        try {
-            const bootcamp = await this._bootcampService.updateById(request.params.id,request.body);
-            if (!bootcamp) {
-                return response.status(400).json({sucess: false , body: bootcamp});
-            }
-
-            response.status(200).json({
-                success:true,
-                message: `Updated Bootcamp ${request.params.id}`,
-                body: bootcamp
-            });
-        } catch (err) {
-            return response.status(400).json({sucess: false , body: err});
-        }
-        
-    };
-
-    deleteById = async (request, response, next) => {
-        try {
-            const bootcamp = await this._bootcampService.deleteById(request.params.id);
-            if (!bootcamp) {
-                return response.status(400).json({sucess: false , body: bootcamp});
-            }
-
-            return response.status(200).json({
-                success: true,
-                message: `Deleted Bootcamp ${request.params.id}`,
-                body: bootcamp
-            });
-        } catch (err) {
-            return response.status(400).json({sucess: false, error: err});
-        }
-    };
+        return response.status(200).json({
+            success: true,
+            message: `Deleted Bootcamp ${request.params.id}`,
+            body: bootcamp
+        });
+    });
 }
 
 module.exports = BootcampController;
---
+++ middleware/asyncHandler.js
@@ -0,0 +1,5 @@
+AsyncHandler = fn => (request, response, next) =>
+    Promise
+        .resolve(fn(request, response, next))
+        .catch(next)
+module.exports = AsyncHandler;
